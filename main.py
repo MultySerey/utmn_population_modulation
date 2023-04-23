729a10728b5117dde72c673d56798223e9314710
Сергей Голованов <multyserey@yandex.ru>
2023-04-23 18:13:50 +0500

    -math +np

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import typing
 
 import numpy as np
@@ -12,7 +11,7 @@ TICK = 1/FPS
 DOT_RADIUS = 20
 HALF_RADIUS = DOT_RADIUS / 2
 
-PI = math.pi
+PI = np.pi
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -22,13 +21,13 @@ BLUE = (0, 0, 255)
 
 
 def vector_length(v: np.ndarray) -> float:
-    return math.sqrt(v[0] ** 2 + v[1] ** 2)
+    return np.sqrt(v[0] ** 2 + v[1] ** 2)
 
 
 def clamp_magnitude(vector, max_length):
     sqrmag: float = vector_length(np.array(vector))
     if sqrmag > max_length ** 2:
-        mag: float = math.sqrt(sqrmag)
+        mag: float = np.sqrt(sqrmag)
         normal_x = vector[0] / mag
         normal_y = vector[1] / mag
         return [normal_x * max_length, normal_y * max_length]
@@ -41,6 +40,12 @@ def normalize(position: np.ndarray, target: np.ndarray):
     return delta_pos / distance
 
 
+def insideUnitCircle():
+    a = np.random.random()
+    b = np.sqrt(1-a**2)
+    return np.array([a, b])
+
+
 class Dot:
     def __init__(self) -> None:
         self.x: float = np.random.random() * 540 + 50
@@ -50,17 +55,25 @@ class Dot:
         self.steer_strength = np.random.random() * 5+5
         self.color = np.random.random(3) * 200 + 50
         self.radius = 10
+        self.wander = 1
 
     @property
     def position(self):
         return [self.x, self.y]
 
 
+class Obstruction:
+    def __init__(self, x0, y0, x1, y1) -> None:
+        self.start_pos = [x0, y0]
+        self.end_pos = [x1, y1]
+
+
 class DotController:
-    def __init__(self, dot_amount: int) -> None:
+    def __init__(self, dot_amount: int, obs_list: typing.List[Obstruction]) -> None:
         self.dot_list: typing.List[Dot] = [Dot() for _ in range(dot_amount)]
-        self.target = self.random_target()
         self.accuracy = 20.0
+        self.obs_list = obs_list
+        self.target = self.random_target()
 
     def __len__(self):
         return len(self.dot_list)
@@ -81,7 +94,16 @@ class DotController:
             raise StopIteration
 
     def random_target(self):
-        return np.random.random(2) * 540 + 50
+        new = None
+        check_list = []
+        while True:
+            new = np.random.random(2) * 540 + 50
+            for obs in self.obs_list:
+                if not (obs.start_pos[0] < new[0] < obs.end_pos[0] and obs.start_pos[1] < new[1] < obs.end_pos[1]):
+                    check_list.append(True)
+            if len(check_list) == len(self.obs_list):
+                break
+        return new
 
     def update(self):
         for dot in self.dot_list:
@@ -112,21 +134,25 @@ clock = pygame.time.Clock()
 
 
 def main():
-    dot_controller = DotController(50)
+    obs = [Obstruction(240, 240, 400, 400)]
+    dot_controller = DotController(50, obs)
 
     running = True
 
     def redraw_window():
+        # for o in obs:
+        #    pygame.draw.lines(screen, RED, True, [o.start_pos, [
+        #        o.start_pos[1], o.end_pos[0]], o.end_pos, [
+        #        o.end_pos[1], o.start_pos[0]]])
         for dot in dot_controller:
             pygame.draw.circle(screen, GREEN, dot_controller.target,
                                dot_controller.accuracy, 1)
-            # pygame.draw.line(screen, GREEN, dot.position,
-            #                   dot_controller.target, 1)
+
             pygame.draw.circle(screen, dot.color, dot.position, 10, 1)
-            atan = math.atan2(
+            atan = np.arctan2(
                 dot_controller.target[1]-dot.y, dot_controller.target[0]-dot.x)
             pygame.draw.line(screen, dot.color, dot.position,
-                             (dot.x+math.cos(atan)*40, dot.y+math.sin(atan)*40), 1)
+                             (dot.x+np.cos(atan)*40, dot.y+np.sin(atan)*40), 1)
         pygame.display.update()
 
     ticker = 1
